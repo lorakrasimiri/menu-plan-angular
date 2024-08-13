ad7edf2798a6e7c7dfee256f2ede76623ec3c18c
lora krasimi <loraadova@gmail.com>
2024-08-13 17:39:42 +0200

    angular material install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 
@@ -25,7 +26,9 @@ import { AboutUsComponent } from './about-us/about-us.component';
     AppRoutingModule,
   ],
   exports: [AppRoutingModule],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
